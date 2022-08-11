609ea514913995b373eb4375c094a3554d6dfcc3
Nastia Kartseva <danetzachem@gmail.com>
2022-08-11 12:55:51 +0300

    update root component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import OrderProvider from '../components/OrderProvider.jsx';
+import ApodProvider from '../components/ApodProvider.jsx';
+import AsteroidsProvider from '../components/AsteroidsProvider';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApodProvider>
+      <AsteroidsProvider>
+        <OrderProvider>
+          <Component {...pageProps} />
+        </OrderProvider>
+      </AsteroidsProvider>
+    </ApodProvider>
+  );
 }
 
 export default MyApp
